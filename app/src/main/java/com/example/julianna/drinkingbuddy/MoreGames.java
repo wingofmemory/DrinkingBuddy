a1f4c4a42ba54cfe87ecedb61bd8b6db00fb0ea2
Julianna <pjuli007@gmail.com>
2014-12-11 11:50:29 +0100

    Description to President is added

--- app/src/main/java/com/example/julianna/drinkingbuddy/MoreGames.java
+++ app/src/main/java/com/example/julianna/drinkingbuddy/MoreGames.java
@@ -36,8 +36,7 @@ public class MoreGames {
                   "Duck Sauce – Big Bad Wolf – Drikk hver gang de synger “Big Bad Wolf”\n" +
                   "Daft Punk – Around The World – Drikk hver gang de synger “Around the world”\n" +
                   "Red Hot Chili Peppers – Give It Away – Drikk hver gang de synger “Give it away”\n" +
-                  "Fatboy Slim, Riva Starr & Beardyman – Eat Sleep Rave Repeat – Drikk hver gang de synger “Eat sleep rave repeat”\n"
-                    ,
+                  "Fatboy Slim, Riva Starr & Beardyman – Eat Sleep Rave Repeat – Drikk hver gang de synger “Eat sleep rave repeat”\n",
 
             "Du trenger: Kortstokk og alkohol \n" + "\n" +
             "Legg kortene i en pyramide med bildet ned på følgende måte:\n" +
@@ -75,14 +74,42 @@ public class MoreGames {
     "Så må han/hun begynne på nytt og trekke fra bunn, helt til han/hun er kommet til toppen uten å få et bildekort. \n" +
     "Bildekort er Ess, Konge, Dame og Knekt. \n",
 
+    "Du trenger: en kortstokk \n" +
+    "Antall spillere: 3-5 (best med 4) \n" +
+    "Antall kort: Hele kortstokken (Jokere kan også brukes)\n" +
+    " Alle kortene skal deles ut, det gjør ikke noe om ikke alle får nøyaktig like mange kort. \n" +
+    "Kortenes verdi: 2 er høyest, ess nest høyest, deretter kommer Konge, Dame, knekt, ti, ni, åtte, sju, fem, fire, og lavest er tre." +
+    "Farge har ingen betydning. Seksere og jokere kan erstatte andre kort. \n" +
+    "Meningen med spillet: Å bli kvitt kortene så fort som mulig. \n" +
+    "Slik spiller man: Første runde starter den som har kløver tre å spille ut. " +
+    "I neste runde er det den som tapte forrige gang som begynner." +
+    "Den som begynner spiller ut ett eller flere like kort. \n" +
+    "For eksempel to firere. Neste spiller må spille ut like mange kort som den som begynte, " +
+            "men de må være høyere, eller like høye. Han kan for eksempel spille ut to femmere. " +
+            "Slik fortsetter det rundt bordet til alle spillerne har spilt ut kort. " +
+            "Den som spilte ut sist (og dermed spilte ut de høyeste kortene) får stikket, og skal spille ut de(t) neste kortene. " +
+            "Det har imidlertid ingen betydning hvem som får stikket. " +
+            "Det er derfor vanlig at når et stikk er avsluttet, legges alle kortene bort i en felles haug. \n" +
+    "Hvis den som vant stikket ikke har flere kort igjen, er det spilleren til venstre for han som skal spille ut de(t) neste kortene. \n" +
+    "Hvis man ikke kan (eller vil) spille ut høyere kort enn de forrige spillerne, sier man pass. " +
+            "Den som melder pass får ikke spilt ut noe kort i dette stikket.\n" +
+    "Hvis noen for eksempel har spilt fire konger, kan neste spiller spille ut to ess og to seksere. " +
+            "Seksere og jokere kan her erstatte hvilket som helst kort. Også fire seksere kunne dermed vært spilt i dette eksemplet.\n" +
+    "Men hvis den som begynner stikket spiller ut bare seksere (eller jokere), har disse verdien seks." +
+            " Han kan ikke da velge at de for eksempel skal tilsvare toere. \n" +
+    "Slutt: Den som først blir kvitt sine kort har vunnet. Nestemann blir nummer 2, osv. " +
+            "Dere må altså fortsette til det bare er én spiller igjen. \n" +
+    "Neste runde: Den som vinner får tittelen President. Nummer to blir Visepresident. \n" +
+    "Den som taper blir slave, og den nest dårligste blir viseslave. " +
+            "(Hvis det bare er tre spillere, blir ingen visepresident eller viseslave) \n" +
+    "Etter at kortene til neste runde er delt ut, skjer følgende før dere begynner å spille: \n" +
+    "-Presidenten skal ha de to beste kortene fra slaven, mens han gir tilbake to valgfrie kort til slaven. " +
+            "Presidenten kan også spørre etter noen spesielle kort. \n" +
+    "-Visepresidenten skal ha det beste kortet fra viseslaven, mens han gir tilbake et valgfritt kort." +
+            " Visepresidenten kan også spørre etter et spesielt kort. \n",
 
 
-            };
-
-
-
-
-
+};
 
 
 
